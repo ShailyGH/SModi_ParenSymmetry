c653b711d98e17d7181fc1819650decaaae01e51
ShailyGH <shailykunal@.com>
2024-03-01 20:58:11 -0500

    finished

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,16 +1,56 @@
 package src.main.java;
-
+import java.io.File; //Import the File class
+import java.io.FileNotFoundException;
+import java.util.Scanner; // Import the Scanner class to read text files.
 public class ParenSymmetry {
 
 
     private Boolean isBalanced(String s) {
         // implement this method
-        return null;
+        char openParen = '(';
+        char closeParen = ')';
+        int countOpenParen = 0;
+        int countCloseParen = 0;
+        Boolean val = false;
+
+        for (int i = 0; i< s.length(); i++)
+        {
+            if (s.charAt(i) == openParen)
+            {
+                countOpenParen++;
+            }
+            else if (s.charAt(i) == closeParen)
+            {
+                countCloseParen++;
+            }
+        }
+
+        if (countOpenParen == countCloseParen)
+        {
+           val = true;
+        }
+        return val;
     }
 
     private void checkFile(String filename) {
         // open file named filename
+        String output = "";
 
+        try {
+            File myFile1 = new File(filename);
+            Scanner myReader = new Scanner(myFile1);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                output = output + isBalanced(data).toString() + ", ";
+            }
+            myReader.close();
+            System.out.println(output);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("File Not Found");
+            e.printStackTrace();
+        }
         // for each line in the file
             // read the line
             // print whether or not the line's parenthesis are balanced
@@ -20,7 +60,11 @@ public class ParenSymmetry {
 
     public static void main(String[] args) {
         ParenSymmetry ps = new ParenSymmetry();
-
+        System.out.println("\nOutput from file - TestStrings0.txt");
+        ps.checkFile("TestStrings0.txt");
+        System.out.println("\nOutput from file - TestStrings1.txt");
+        ps.checkFile("TestStrings1.txt");
+        /*
         Boolean b0 = ps.isBalanced("()");
         printResult(b0, true);
 
@@ -37,7 +81,7 @@ public class ParenSymmetry {
             trues = ps.isBalanced(strToTest);
         }
         printResult(trues, true);
-
+        */
     }
 
     private static void printResult(Boolean b0, boolean b) {
